1adcae3643c479b53f861c97869534d162d20911
ZePan110 <ze.pan@intel.com>
2024-10-16 18:03:49 +0800

    Clean docker env and Fix launch service issues.
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- .github/scripts/launch_service/launch_AudioQnA.sh
+++ .github/scripts/launch_service/launch_AudioQnA.sh
@@ -18,4 +18,4 @@ git clone https://github.com/opea-project/GenAIComps
 cd GenAIComps
 docker build -t opea/whisper:acc --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/asr/whisper/Dockerfile .
 # change the name of model by editing model_name_or_path you want to evaluate
-docker run -p 7066:7066 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy opea/whisper:acc --model_name_or_path "openai/whisper-tiny"
+docker run -tid -p 7066:7066 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy opea/whisper:acc --model_name_or_path "openai/whisper-tiny"
