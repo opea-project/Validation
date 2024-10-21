d648a6e55f18c0ce9c1346f1b4feba3aa926070b
ZePan110 <ze.pan@intel.com>
2024-10-21 22:54:37 +0800

    test AudioQnA and Codegen
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- .github/scripts/launch_acc.sh
+++ .github/scripts/launch_acc.sh
@@ -7,13 +7,10 @@ echo $1 $2
 if [[ "$1" == "CodeGen" ]]; then
     export CODEGEN_ENDPOINT="http://${ip_address}:7778/v1/codegen"
     export CODEGEN_MODEL="Qwen/CodeQwen1.5-7B-Chat"
-    cat run_acc.sh
-    cat main.py
     bash run_acc.sh $CODEGEN_MODEL $CODEGEN_ENDPOINT
 elif [[ "$1" == "ChatQnA" ]]; then
     sed -i 's|--docs_path MultiHop-RAG/dataset/corpus.json|--docs_path /data2/opea-dataset/ChatQnA/MultiHop-RAG/dataset/corpus.json|g' run_acc.sh
     sed -i 's|--dataset_path MultiHop-RAG/dataset/MultiHopRAG.json|--dataset_path /data2/opea-dataset/ChatQnA/MultiHop-RAG/dataset/MultiHopRAG.json|g' run_acc.sh
-
     sed -i '/git clone https:\/\/github.com\/yixuantt\/MultiHop-RAG.git/d' run_acc.sh
     sed -i '/git clone https:\/\/github.com\/IAAR-Shanghai\/CRUD_RAG/d' run_acc.sh
     sed -i '/mkdir data\//d' run_acc.sh
