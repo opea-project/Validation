76ede8430445fbd208e6e15bf60da5792b249b71
ZePan110 <ze.pan@intel.com>
2024-10-29 23:54:30 +0800

    test FaqGen
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- .github/scripts/acc_test.sh
+++ .github/scripts/acc_test.sh
@@ -50,14 +50,20 @@ function eval_prepare(){
         [ ! -d "$WORKPATH/GenAIExamples/$1/benchmark/accuracy/data/result" ] && mkdir -p $WORKPATH/GenAIExamples/$1/benchmark/accuracy/data/result
         python generate_FAQ.py
         python post_process_FAQ.py
-        volume=$PWD/data
-        model=meta-llama/Llama-2-7b-hf
-        docker run -tid -p 8082:80 -v $volume:/data --runtime=habana -e HABANA_VISIBLE_DEVICES=all  -e PT_HPU_LAZY_MODE=0 -e OMPI_MCA_btl_vader_single_copy_mechanism=none  -e HF_TOKEN=$HF_TOKEN --cap-add=sys_nice --ipc=host  ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model --max-input-tokens 3072 --max-total-tokens 4096 
-        # export LLM_ENDPOINT="http://${ip_address}:8082"
-        # curl http://${ip_address}:8082/generate \
-        #   -X POST \
-        #   -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128}}' \
-        #   -H 'Content-Type: application/json'
+        # volume=$PWD/data
+        # model=meta-llama/Llama-2-7b-hf
+        # docker run -tid -p 8082:80 -v $volume:/data --runtime=habana -e HABANA_VISIBLE_DEVICES=all  -e PT_HPU_LAZY_MODE=0 -e OMPI_MCA_btl_vader_single_copy_mechanism=none  -e HF_TOKEN=$HF_TOKEN --cap-add=sys_nice --ipc=host  ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model --max-input-tokens 3072 --max-total-tokens 4096 
+        max_input_tokens=3072
+        max_total_tokens=4096
+        port_number=8082
+        model_name="mistralai/Mixtral-8x7B-Instruct-v0.1"
+        volume="./data"
+        docker run -dit --rm --name="tgi_Mixtral" -p $port_number:80 -v $volume:/data --runtime=habana --restart always -e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true --cap-add=sys_nice --ipc=host -e HTTPS_PROXY=$https_proxy -e HTTP_PROXY=$https_proxy ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model_name --max-input-tokens $max_input_tokens --max-total-tokens $max_total_tokens --sharded true --num-shard 2
+        export LLM_ENDPOINT="http://${ip_address}:8082"
+        curl http://${ip_address}:8082/generate \
+          -X POST \
+          -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":128}}' \
+          -H 'Content-Type: application/json'
     fi
 }
 
