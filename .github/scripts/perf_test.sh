86aad5dbee8d9df473bedfb62e3e8d5c3d28a338
chensuyue <suyue.chen@intel.com>
2024-11-27 09:44:32 +0800

    update sleep time
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .github/scripts/perf_test.sh
+++ .github/scripts/perf_test.sh
@@ -104,12 +104,12 @@ function installChatQnA() {
         curl -X POST "http://${dataprep_host}:6007/v1/dataprep" \
            -H "Content-Type: multipart/form-data" \
            -F "files=@./upload_file_no_rerank.txt" \
-	   -F "chunk_size=3800"
+	         -F "chunk_size=3800"
     else
         curl -X POST "http://${dataprep_host}:6007/v1/dataprep" \
            -H "Content-Type: multipart/form-data" \
            -F "files=@./upload_file.txt" \
-	   -F "chunk_size=3800"
+	         -F "chunk_size=3800"
     fi
     popd
 }
