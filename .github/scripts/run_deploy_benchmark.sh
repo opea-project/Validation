011c9fe0bb81be60a247aa667591ba31e48ada34
chensuyue <suyue.chen@intel.com>
2025-02-17 23:58:36 +0800

    support run deploy and benchmark one in all
    Signed-off-by: chensuyue <suyue.chen@intel.com>

---
+++ .github/scripts/run_deploy_benchmark.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+function generate_config(){
+    echo "Generating deploy and benchmark config..."
+    example=$1
+    deploy_args=$2
+    benchmark_args=$3
+
+    lower_example=$(echo "$example" | tr '[:upper:]' '[:lower:]')
+    example_yaml_path="./$example/benchmark_$lower_example.yaml"
+    if [ -f $example_yaml_path ]; then
+        echo "Found example yaml file: $example_yaml_path"
+    else
+        echo "Example yaml file not found: $example_yaml_path"
+        exit 1
+    fi
+
+    # update model path and hf_token
+    sed -i "s/^\s*modelUseHostPath:.*/modelUseHostPath: $HOME/$MODEL_PATH/" "$example_yaml_path"
+    sed -i "s/^\s*HUGGINGFACEHUB_API_TOKEN:.*/HUGGINGFACEHUB_API_TOKEN: $HF_TOKEN/" "$example_yaml_path"
+
+    # update other input args
+    update_yaml "$example_yaml_path" "$deploy_args"
+    update_yaml "$example_yaml_path" "$benchmark_args"
+
+    echo "Print update config yaml..."
+    cat "$example_yaml_path"
+
+}
+
+function update_yaml() {
+    yaml_path=$1
+    args=$2
+
+    IFS=',' read -ra pairs <<< "$args"
+    for pair in "${pairs[@]}"; do
+      IFS='=' read -ra kv <<< "$pair"
+      key="${kv[0]}"
+      value="${kv[1]}"
+
+      if [ "$key" = "with_rerank" ]; then
+          sed -i "s/^\s*enabled:.*/enabled: $value/" "$yaml_path"
+      else
+          sed -i "s/^\s*$key:.*/$key: $value/" "$yaml_path"
+      fi
+    done
+}
+
+function run() {
+    echo "Run deploy and benchmark..."
+    example=$1
+    test_mode=$2
+
+    lower_example=$(echo "$example" | tr '[:upper:]' '[:lower:]')
+    example_yaml_path="./$example/benchmark_$lower_example.yaml"
+    python deploy_and_benchmark.py "${example_yaml_path}" --test-mode "${test_mode}"
+}
+
+# Process the options
+case "$1" in
+    --generate_config)
+        pushd ../GenAIExamples
+        generate_config $2 $3 $4
+        popd
+        ;;
+    --run)
+        pushd ../GenAIExamples
+        run $2 $3
+        popd
+        ;;
+    *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+esac
