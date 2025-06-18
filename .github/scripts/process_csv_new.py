db7fbcca8f21a47ef96ed1e486f410a83cd64fc1
chensuyue <suyue.chen@intel.com>
2025-06-18 13:38:22 +0800

    only keep Avg and P99 for the benchmark output
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .github/scripts/process_csv_new.py
+++ .github/scripts/process_csv_new.py
@@ -11,23 +11,11 @@ csv_file = args.csv_file
 
 columns_to_keep = [
     'End_to_End_latency_Avg',
-    'End_to_End_latency_P50',
-    'End_to_End_latency_P90',
     'End_to_End_latency_P99',
     'Time_to_First_Token-TTFT_Avg',
-    'Time_to_First_Token-TTFT_P50',
-    'Time_to_First_Token-TTFT_P90',
     'Time_to_First_Token-TTFT_P99',
     'Time_Per_Output_Token-TPOT_Avg',
-    'Time_Per_Output_Token-TPOT_P50',
-    'Time_Per_Output_Token-TPOT_P90',
     'Time_Per_Output_Token-TPOT_P99',
-    'Output_Tokens_per_Second',
-    'Input_Tokens_per_Second',
-    'Onput_Tokens',
-    'Input_Tokens',
-    'Average_token_latency',
-    'Duration',
 ]
 
 df = pd.read_csv(csv_file, delimiter=',')
